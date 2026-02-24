9f6cbace235560bb5ba7b19db263d4397dd9d589
NaniDerEchte2 <nani2003@gmx.de>
2026-02-24 23:19:15 +0100

    Remove embedded Steam cogs (use external repo)

--- cogs/steam/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Steam cog package exports."""
-
-from __future__ import annotations
-
-# Alle Schnelllink-Funktionalität wurde entfernt.
-# Das System verwendet jetzt ausschließlich OAuth für Steam-Account-Verknüpfungen.
-
-__all__ = []
